5b156c9db637d4afb5f3c70109cc2f3b167871c1
Ingo Strauch <ingostrauch@eurofins.de>
2016-04-13 13:41:03 +0200

    Added files

---
+++ InvoicesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Mvc;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceWebApi
+{
+    [Route("/api/invoices")]
+    public class InvoicesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Invoice> Get()
+        {
+            using (var conn = new NpgsqlConnection("Host=pgdb;Username=postgres;Password=P@ssw0rd!"))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    // Insert some data
+                    cmd.CommandText = "SELECT * FROM Invoices;";
+                    var result = new List<Invoice>();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(new Invoice
+                            {
+                                ID = reader.GetInt32(0),
+                                Description = reader.GetString(1),
+                                Customer = reader.GetString(2)
+                            });
+                        }
+
+                        return result;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceWebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.UseMvc();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
